58cdec82d2d2d95e182bc636dc63a21cde0b430c
Luc Mosser <luc.mosser86@orange.fr>
2020-06-19 09:42:18 +0200

    searching bar

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class VideoDetail extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+
+export default VideoDetail;
